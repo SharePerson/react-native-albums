0b8d71e8767d5bff616fd3dd28c57a60fb00bd50
Fady Soliman <fadysoliman160@hotmail.com>
2018-05-08 10:29:24 +0200

    components development

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+//define imports
+import React from 'react';
+import { Text } from 'react-native';
+
+// create the Component
+ const Header = () => {
+  return <Text>Albums!</Text>;
+};
+
+//export the component to make it available for other components in the app
+export default Header;
